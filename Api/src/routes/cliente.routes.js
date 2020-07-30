a845543488b8ad8eacedbd13712e96fc114f6c6e
Gabriel Silva de Azevedo <gabrielsilvadeazevedo@gmail.com>
2020-07-29 23:01:34 -0300

    Revert "initial commit with some part of the back end done"
    This reverts commit 133f2b0eff3897828370651fa06fd105ac93d0f1.

--- Api/src/routes/cliente.routes.js
+++
@@ -1,7 +0,0 @@
-const router = require('express-promise-router')();
-const clienteController = require('../controllers/cliente.controller');
-
-
-router.post('/clientes', clienteController.createCliente);
-
-module.exports = router;
--- Api/src/routes/pedido.routes.js
+++
@@ -1,9 +0,0 @@
-const router = require('express-promise-router')();
-const pedidoController = require('../controllers/pedido.controller');
-
-router.post('/pedidos', pedidoController.createPedido);
-router.get('/pedidos/', pedidoController.listarAllPedidos);
-router.get('/pedidos/naoarquivados', pedidoController.listarAllPedidosNaoArquivados);
-router.get('/pedidos/arquivados', pedidoController.listarAllPedidosArquivados);
-
-module.exports = router;
